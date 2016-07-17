bb74bb1d3bdb3b9ab1e98489ff61ff0fc2446e2e
Alfred Lotho <alfred.lotho@gmail.com>
2016-07-17 18:24:38 +0900

    add other methods for managing Photo instances such as save, destroy, find

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -37,4 +37,42 @@ class Photo
     end
   end
 
+  def self.all(offset=0, limit=nil)
+    photos=[]
+    documents = mongo_client.database.fs.find.skip(offset)
+    documents = documents.limit(limit) if !limit.nil?
+    documents.each do |doc| 
+      photos << Photo.new(doc)
+    end
+    return photos
+  end
+
+  def self.id_criteria id
+    {_id:BSON::ObjectId.from_string(id)}
+  end
+
+  def id_criteria
+    self.class.id_criteria @id
+  end
+
+  def self.find id
+    document = mongo_client.database.fs.find(id_criteria(id)).first
+    return document.nil? ? nil : Photo.new(document)
+  end
+
+  def contents
+    document = self.class.mongo_client.database.fs.find_one(id_criteria)
+    if document
+      buffer = ""
+      document.chunks.reduce([]) do |x,chunk| 
+          buffer << chunk.data.data 
+      end
+      return buffer
+    end
+  end
+
+  def destroy
+    self.class.mongo_client.database.fs.find(id_criteria).delete_one
+  end
+
 end
